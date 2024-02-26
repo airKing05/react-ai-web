e36e9cae1d5389fe7f3693a47babdf90282081b0
akshatsZangula <akshat.sandhaliya@zangula.com>
2024-02-26 21:39:51 +0530

    pop-up one by even click

--- src/pages/Calendar/CustomCalendar.jsx
+++ src/pages/Calendar/CustomCalendar.jsx
@@ -9,27 +9,40 @@ const localizer = momentLocalizer(moment);
 
 const events = [
   {
-    start: moment('2024-02-24T18:00:00').toDate(),
-    end: moment('2024-02-24T19:00:00').toDate(),
+    start: moment('2024-02-26T18:00:00').toDate(),
+    end: moment('2024-02-26T19:00:00').toDate(),
     title: 'Call with Yaniv'
   },
   {
-    start: moment('2024-02-24T20:00:00').toDate(),
-    end: moment('2024-02-24T22:00:00').toDate(),
+    start: moment('2024-02-26T20:00:00').toDate(),
+    end: moment('2024-02-26T22:00:00').toDate(),
     title: 'With Sathish'
+  },
+  {
+    start: moment('2024-02-26T21:00:00').toDate(),
+    end: moment('2024-02-26T23:00:00').toDate(),
+    title: 'Deployment'
   }
 ]
 
 export default function CustomCalendar() {
-  const [showEventDetails, setShowEventDetails] = useState(false)
+  const [showEventDetails, setShowEventDetails] = useState(false);
+  const [selectedEvent, setSelectedEvent] = useState(null)
+
+  const handleSelected = (event) => {
+    // console.log("event", event)
+    setSelectedEvent(event);
+    setShowEventDetails(true)
+  };
   return (
     <div className='calender_container'>
-      <div onClick={() => setShowEventDetails(true)}>Show Event Details</div>
+      {/* <div onClick={() => setShowEventDetails(true)}>Show Event Details</div> */}
       <Calendar
         localizer={localizer}
         events={events}
         startAccessor="start"
         endAccessor="end"
+        onSelectEvent={handleSelected}
         // style={{ height: 500 }}
       />
       {
@@ -37,7 +50,10 @@ export default function CustomCalendar() {
       }
       {
         showEventDetails ? <div className='popup_container'>
-          <EventDetailPopup setShowEventDetails={setShowEventDetails}/>
+          <EventDetailPopup 
+          setShowEventDetails={setShowEventDetails}
+          selectedEvent={selectedEvent}
+          />
         </div> : null
       }
     </div>
